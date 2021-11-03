4af8b46e6c099bbe503c1bc3fd8e60b0d6f1693a
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-11-03 11:22:52 +0530

    error fixed v2

--- src/components/Utility.js
+++ src/components/Utility.js
@@ -4,30 +4,30 @@ export class Utility extends Component {
   render() {
     return (
       <div className="util">
-        <a
-          href="#"
+        <button
+          href="#id"
           className="btn"
           onClick={() => this.props.handlePrev()}
           rel="noopener noreferrer"
         >
           <i className="fas fa-arrow-alt-circle-left"></i>
-        </a>
-        <a
-          href="#"
+        </button>
+        <button
+          href="#id"
           onClick={() => this.props.handleReset()}
           className="btn"
           rel="noopener noreferrer"
         >
           <i className="fas fa-redo"></i>
-        </a>
-        <a
-          href="#"
+        </button>
+        <button
+          href="#id"
           onClick={() => this.props.prevClicked ? this.props.handleNext() : ""}
           className="btn"
           rel="noopener noreferrer"
         >
           <i className="fas fa-arrow-alt-circle-right"></i>
-        </a>
+        </button>
       </div>
     );
   }
